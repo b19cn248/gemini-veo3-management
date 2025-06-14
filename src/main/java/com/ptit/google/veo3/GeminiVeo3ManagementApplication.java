1df0d74e634bc026f73edca9753d4640e37dc4c1
hieunguyen <hieunguyen@vnpt.vn>
2025-06-14 22:21:03 +0700

    OKE

--- src/main/java/com/ptit/google/veo3/GeminiVeo3ManagementApplication.java
+++ src/main/java/com/ptit/google/veo3/GeminiVeo3ManagementApplication.java
@@ -3,10 +3,14 @@ package com.ptit.google.veo3;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableAsync
 public class GeminiVeo3ManagementApplication {
 
     public static void main(String[] args) {
