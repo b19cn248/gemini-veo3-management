eb860a26b227c6dc3acb8453b20e1f3d0001498d
hieunguyen <hieunguyen@vnpt.vn>
2025-06-17 09:25:15 +0700

    OKE

--- src/main/java/com/ptit/google/veo3/entity/DeliveryStatus.java
+++ src/main/java/com/ptit/google/veo3/entity/DeliveryStatus.java
@@ -3,7 +3,8 @@ package com.ptit.google.veo3.entity;
 public enum DeliveryStatus {
     DA_GUI("Đã gửi"),
     CHUA_GUI("Chưa gửi"),
-    CAN_SUA_GAP("Cần sửa gấp");
+    CAN_SUA_GAP("Cần sửa gấp"),
+    HUY("Huỷ");
 
     private final String displayName;
 
